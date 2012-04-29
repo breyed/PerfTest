0cfaaf111def931b8b636b7817e7a5d7e5df377a
Edward Brey <EdAndBeth@BreyFamily.net>
2012-04-29 06:40:48 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PerfTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			int count = 0;
+			for (uint i = 0; i < 1000000000; ++i) {
+				var isMultipleOf16 = i % 16 == 0;
+				count += isMultipleOf16 ? 1 : 0;
+				//count += i % 16 == 0 ? 1 : 0;
+			}
+			stopwatch.Stop();
+			Console.WriteLine("Count: {0}, Time: {1}", count, stopwatch.ElapsedMilliseconds);
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Simple .NET 4.0 performance test")]
+[assembly: AssemblyTitle("Simple .NET 4.0 performance test")]
+[assembly: AssemblyDescription("Demonstrates a case where code does not get optimized.")]
+[assembly: AssemblyCompany("Edward D. Brey")]
+[assembly: AssemblyCopyright("Public domain")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
