3d5662e51604648a4060130cdb74ad3f4ac519e7
Edward Brey <EdAndBeth@BreyFamily.net>
2012-04-29 06:48:18 -0700

    added both cases to test; switched to x64

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace PerfTest
 	{
 		static void Main(string[] args)
 		{
+			SingleLineTest();
+			MultiLineTest();
+			SingleLineTest();
+			MultiLineTest();
+			SingleLineTest();
+			MultiLineTest();
+		}
+
+		public static void SingleLineTest()
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			int count = 0;
+			for (uint i = 0; i < 1000000000; ++i) {
+				//var isMultipleOf16 = i % 16 == 0;
+				//count += isMultipleOf16 ? 1 : 0;
+				count += i % 16 == 0 ? 1 : 0;
+			}
+			stopwatch.Stop();
+			Console.WriteLine("Single-line test --> Count: {0}, Time: {1}", count, stopwatch.ElapsedMilliseconds);
+		}
+
+		public static void MultiLineTest()
+		{
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
 			int count = 0;
@@ -19,7 +43,7 @@ namespace PerfTest
 				//count += i % 16 == 0 ? 1 : 0;
 			}
 			stopwatch.Stop();
-			Console.WriteLine("Count: {0}, Time: {1}", count, stopwatch.ElapsedMilliseconds);
+			Console.WriteLine("Multi-line test  --> Count: {0}, Time: {1}", count, stopwatch.ElapsedMilliseconds);
 		}
 	}
 }
